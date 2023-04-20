217f6fca85cd426251c48f625c47879e6d186a1e
giovannijunseokim <kingjunseo@gmail.com>
2023-04-20 16:13:48 +0900

    [chore] My Card 문자열 리소스 추출

--- week1/DiceRoller/mycard/src/main/java/com/android/practice/mycard/MainActivity.kt
+++ week1/DiceRoller/mycard/src/main/java/com/android/practice/mycard/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,13 +50,13 @@ fun MtyCardWithImageAndTexts(modifier: Modifier = Modifier) {
             modifier = modifier.fillMaxWidth(0.5f)
         )
         Text(
-            text = "Giovanni Junseo Kim",
+            text = stringResource(R.string.profile_name),
             fontSize = 28.sp,
             fontWeight = FontWeight.Bold,
             color = colorResource(id = R.color.green_android_3ddc84),
             modifier = modifier.padding(14.dp)
         )
-        Text(text = "Android Potato Developer", fontSize = 22.sp, color = Color.White)
+        Text(text = stringResource(R.string.profile_job), fontSize = 22.sp, color = Color.White)
         Spacer(modifier = modifier.fillMaxHeight(0.1f))
         Divider()
         Row(
@@ -69,7 +70,7 @@ fun MtyCardWithImageAndTexts(modifier: Modifier = Modifier) {
                 modifier.padding(top = 10.dp, bottom = 10.dp, end = 20.dp),
                 colorFilter = ColorFilter.tint(colorResource(id = R.color.green_android_3ddc84))
             )
-            Text(text = "010-1234-5678", color = Color.White)
+            Text(text = stringResource(R.string.profile_phone_number), color = Color.White)
         }
         Divider()
         Row(
@@ -83,7 +84,7 @@ fun MtyCardWithImageAndTexts(modifier: Modifier = Modifier) {
                 modifier.padding(top = 10.dp, bottom = 10.dp, end = 20.dp),
                 colorFilter = ColorFilter.tint(colorResource(id = R.color.green_android_3ddc84))
             )
-            Text(text = "share to your friends", color = Color.White)
+            Text(text = stringResource(R.string.profile_share_desc), color = Color.White)
         }
         Divider()
         Row(
@@ -97,7 +98,7 @@ fun MtyCardWithImageAndTexts(modifier: Modifier = Modifier) {
                 modifier.padding(top = 10.dp, bottom = 10.dp, end = 20.dp),
                 colorFilter = ColorFilter.tint(colorResource(id = R.color.green_android_3ddc84))
             )
-            Text(text = "junseojjang@google.com", color = Color.White)
+            Text(text = stringResource(R.string.profile_email), color = Color.White)
         }
     }
 }
