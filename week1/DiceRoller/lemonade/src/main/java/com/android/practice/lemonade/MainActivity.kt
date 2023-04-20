1e5078af9e3eff2cef9674e8535398d28476294a
giovannijunseokim <kingjunseo@gmail.com>
2023-04-20 16:55:38 +0900

    [ui] Lemonade UI 구현

--- week1/DiceRoller/lemonade/src/main/java/com/android/practice/lemonade/MainActivity.kt
+++ week1/DiceRoller/lemonade/src/main/java/com/android/practice/lemonade/MainActivity.kt
@@ -3,13 +3,25 @@ package com.android.practice.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.android.practice.lemonade.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +29,53 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DiceRollerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                LemonadeApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Lemonade(modifier: Modifier = Modifier) {
+    val stage = remember {
+        mutableStateOf(1)
+    }
+    val imageResource = when (stage.value) {
+        1 -> R.drawable.img_lemon_tree
+        2 -> R.drawable.img_lemon_squeeze
+        3 -> R.drawable.img_lemon_drink
+        4 -> R.drawable.img_lemon_restart
+        else -> {
+            stage.value = 1
+            R.drawable.img_lemon_tree
+        }
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = stringResource(id = R.string.lemonade_first), modifier.padding(bottom = 10.dp))
+        Image(
+            painter = painterResource(id = imageResource),
+            contentDescription = stringResource(
+                id = R.string.lemonade_first_description
+            ),
+            modifier = modifier
+                .border(
+                    width = 2.dp, shape = RoundedCornerShape(4.dp), color = colorResource(
+                        id = R.color.blue_69cdd8
+                    )
+                )
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun LemonadeApp() {
     DiceRollerTheme {
-        Greeting("Android")
+        Lemonade()
     }
 }
