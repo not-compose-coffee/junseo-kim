e541c183730a35ed0a2569cf2a87c05b2ff7de30
giovannijunseokim <kingjunseo@gmail.com>
2023-05-01 20:52:33 +0900

    [ui] EditField 구현

--- week2/practice/TipTime/app/src/main/java/com/android/practice/tiptime/MainActivity.kt
+++ week2/practice/TipTime/app/src/main/java/com/android/practice/tiptime/MainActivity.kt
@@ -5,19 +5,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.android.practice.tiptime.ui.theme.TipTimeTheme
+import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,17 +51,37 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TipTimeScreen(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(32.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        modifier = modifier.padding(32.dp), verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         Text(
             text = stringResource(id = R.string.calculate_tip),
             fontSize = 24.sp,
             modifier = modifier.align(Alignment.CenterHorizontally)
         )
+        Spacer(modifier = modifier.height(16.dp))
+        EditNumberField()
+        Spacer(modifier = modifier.height(24.dp))
+        Text(
+            modifier = modifier.align(Alignment.CenterHorizontally),
+            text = stringResource(id = R.string.tip_amount, ""),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun EditNumberField() {
+    var amountInput by remember { mutableStateOf("") }
+    TextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = amountInput,
+        onValueChange = { amountInput = it },
+        label = { Text(text = stringResource(id = R.string.cost_of_service)) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        singleLine = true
+    )
 }
 
 @Preview(showBackground = true)
